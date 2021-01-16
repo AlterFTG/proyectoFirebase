25ea378ac341132f2a574497087cdcfd036c26eb
Facundo <bestjayceeu@gmail.com>
2021-01-16 18:54:51 +0100

    Creamos una BBDD no relacional donde guardaremos a los usuarios que hagan login.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var provider = new firebase.auth.GoogleAuthProvider();
 $('#login').click(function(){
     firebase.auth().signInWithPopup(provider).then(function(result) {
         console.log(result.user);
+        saveUserData(result.user);
     //Dentro de then es cuando tenemos el permiso
     $('#login').hide();
     $('#root').append("<img src='"+result.user.photoURL+"'/>");
@@ -11,3 +12,27 @@ $('#login').click(function(){
     });
 
 });
+
+//Escribir en la base de datos
+//Estamos creando una rama/tabla de BBDD
+$('#save').click(function(){
+    firebase.database().ref("facus")
+    .set({
+        nombre:"Facundo",
+        edad:"15",
+        sexo:"hombretón"
+    })
+})
+
+//Esta función guarda los datos automáticamente
+function saveUserData(user){
+    var user = {
+        uid:user.uid,
+        nombre:user.displayName,
+        email:user.email,
+        foto:user.photoURL
+    }
+    firebase.database().ref("usuarios")
+    .push(user)
+    }
+
